663f9b4fbdafece8ac5090ac4e24bdfda93d9a17
stepan <stepan.granat@kiwi.com>
2021-04-14 21:36:08 +0200

    feat: inital commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Test from "./Test.svelte";
+</script>
+
+<main>
+  <Test />
+</main>
+
+<style>
+  main {
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/Test.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  let items = ["orange", "apple", "peach"];
+
+  $: nicerItems = items.map((i) => `nice ${i}`);
+
+  let newItem = "";
+
+  const addItem = (e) => {
+    e.preventDefault();
+    if (newItem) {
+      items = [...items, newItem];
+      newItem = "";
+    }
+  };
+
+  const removeItem = (index: number) => {
+    items = items.filter((_, i) => i !== index);
+  };
+</script>
+
+<h1>Yo showing items:</h1>
+<ul>
+  {#each nicerItems as item, i}
+    <li on:click={() => removeItem(i)}>{item}</li>
+  {/each}
+</ul>
+
+<form on:submit={addItem}>
+  <input bind:value={newItem} />
+  <button type="submit">Add item</button>
+</form>
